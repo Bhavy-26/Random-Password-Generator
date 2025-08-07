47d11acb60b9cfda4693d7b9bb202b3e093c3a37
Bhavy-26 <154998223+Bhavy-26@users.noreply.github.com>
2025-08-07 10:39:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+import React,{useState,useEffect} from "react";
+import ReactDOM from "react-dom/client"
+
+
+function PasswordGenerator(){
+
+    const [Password,setPassword]=useState("");
+    const[length,setlength]=useState(10);
+    const[numchanged,setnumchanged]=useState(false);
+    const[charchanged,setcharchanged]=useState(false);
+
+    function generatepassword(){
+
+        let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        if(numchanged)
+            str+="0123456789";
+        
+        if(charchanged)
+            str+="@!~\{}()*&#$%^+-''/;:<>=.,";
+
+        let pass="";
+
+        for(let i=0;i<length;i++){
+            pass+=str[Math.floor(Math.random()*str.length)]
+        }
+
+        setPassword(pass);
+
+    }
+
+    // generatepassword()  // yeh maut ka khel hai bhai console mar gya
+
+    useEffect(()=>{
+        generatepassword();
+    },[length,numchanged,charchanged])       
+
+    // password update nhi ho rha? 
+
+    return(
+
+        <>
+        
+            <div className="first">
+
+                     <h1>{Password}</h1>
+
+                            <div className="second">
+
+                                <input type="range" min={5} max={50} value={length} onChange={(e)=>setlength(e.target.value)}></input>
+                                <label>Length({length})</label>
+
+                                <input type="checkbox" defaultChecked={numchanged} onChange={()=> setnumchanged(!numchanged)}></input>
+                                <label>Number</label>
+
+                                <input type="checkbox" defaultChecked={charchanged} onChange={()=> setcharchanged(!charchanged)}></input>
+                                <label>Character</label>
+
+
+                            </div>
+        
+            </div>
+
+        </>
+
+
+    )
+
+
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<PasswordGenerator/>)
